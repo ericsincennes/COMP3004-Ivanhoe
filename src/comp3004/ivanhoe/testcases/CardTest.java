4a901558612f3d053d4e9ed872f53b412b07bef0
Mike Zhang <ifarmpandas@gmail.com>
2016-02-07 15:38:33 -0500

    eclipse compatible?

---
+++ src/comp3004/ivanhoe/testcases/CardTest.java
@@ -0,0 +1,21 @@
+package comp3004.ivanhoe.testcases;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class CardTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
