01839d0b5562add2c1e33e52584f1ddd7b585d64
Shray <Shray@192.168.0.22>
2016-02-09 13:55:36 -0500

    Card
    Now an abstract class which can be used to make all other cards in the
    game

--- src/comp3004/ivanhoe/Card.java
+++ src/comp3004/ivanhoe/Card.java
@@ -1,6 +1,50 @@
 package comp3004.ivanhoe;
 
-public class Card {
-    String type;
+public abstract class Card {
+	protected int value;
+	protected String colour;
+	protected String cardName;
+	protected CardType cardtype;
+	
+	public Card(int value, String colour, String name, CardType type){
+		this.value = value;
+		this.colour = colour;
+		this.cardName = name;
+		this.cardtype = type;
+	}
+	
+	public CardType getCardtype() {
+		return cardtype;
+	}
 
+
+	public void setCardtype(CardType cardtype) {
+		this.cardtype = cardtype;
+	}
+
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getCardName() {
+		return cardName;
+	}
+
+	public void setCardName(String cardName) {
+		this.cardName = cardName;
+	}
+	
 }
