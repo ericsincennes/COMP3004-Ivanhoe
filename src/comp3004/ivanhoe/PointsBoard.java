bd717e72db81a3eaf804c2ae26f34d215e8d89de
Mike Zhang <ifarmpandas@gmail.com>
2016-02-17 17:46:59 -0500

    pointsboard tests

--- src/comp3004/ivanhoe/PointsBoard.java
+++ src/comp3004/ivanhoe/PointsBoard.java
@@ -1,5 +1,33 @@
 package comp3004.ivanhoe;
 
-public class PointsBoard {
+import java.util.ArrayList;
+import java.util.List;
 
+import comp3004.ivanhoe.Card.CardColour;
+
+
+public class PointsBoard {
+	private List<Card> cardsPlayed;
+	private List<Card> actionsPlayed;
+	private int score;
+	private CardColour tourneyColour;
+	
+	public PointsBoard() {
+		cardsPlayed = new ArrayList<Card>();
+		cardsPlayed = new ArrayList<Card>();
+		score = 0;
+	}
+	
+	public PointsBoard(CardColour c) {
+		this();
+		tourneyColour = c;	
+	}
+	
+	public CardColour getColour() {
+		return tourneyColour;
+	}
+	
+	public void setColour(CardColour c) {
+		tourneyColour = c;
+	}
 }
