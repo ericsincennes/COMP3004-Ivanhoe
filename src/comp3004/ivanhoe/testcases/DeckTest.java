99697cfddf070150c03bb6b9726073bcd6d092c4
Eric <phoenix85@live.com>
2016-02-19 00:30:22 -0500

    deck test

---
+++ src/comp3004/ivanhoe/testcases/DeckTest.java
@@ -0,0 +1,21 @@
+package comp3004.ivanhoe.testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import comp3004.ivanhoe.*;
+import comp3004.ivanhoe.Deck;
+
+public class DeckTest {
+	Deck d = new Deck();
+
+	@Test
+	public void createDeck() {
+		assertTrue(d.cards != null);
+		assertTrue();
+	}
+	
+	@Test
+	
+	
+}
