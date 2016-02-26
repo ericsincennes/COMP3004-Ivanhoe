6f7f7211543b9b2a6e19bc35f6d801e8afec9785
Shray <Shray@Shray-PC>
2016-02-26 15:55:57 -0500

    Rules engine Choose first tourney colour, register players, deal hand test

--- src/comp3004/ivanhoe/testcases/RulesEngineTest.java
+++ src/comp3004/ivanhoe/testcases/RulesEngineTest.java
@@ -1,37 +1,45 @@
 package comp3004.ivanhoe.testcases;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
 import comp3004.ivanhoe.*;
+import comp3004.ivanhoe.Card.CardColour;
 
 public class RulesEngineTest {
+	RulesEngine e;
 	
-	@Test
-	public void RegisterThread(){
-		Client c = new Client();		
-		assertTrue(c.getPlayerNum() != -1);
+	@Before
+	public void setUp() throws Exception {
+		e = new RulesEngine(5);
 	}
 	
 	@Test
 	public void choosefirstTournement(){
-		RulesEngine e = new RulesEngine(5);
 		e.registerThread(10);
 		e.registerThread(11);
 		e.registerThread(12);
 		e.registerThread(13);
 		e.registerThread(14);
 		
-		long a = e.firstTournament();
-		assertTrue(a != -1);
+		long a = e.chooseFirstTournament();
+		assertTrue(a >= 10 && a <= 14);
 	}
 	
 	@Test
-	public void getTournementChoice(){
+	public void InitTourneyColours(){
+		choosefirstTournement();
 		
+		e.initializeTournementColour(CardColour.Blue);
+		assertEquals(CardColour.Blue, e.getPlayerById(10).getDisplay().getColour());
 	}
 	
-	
-	
+	@Test
+	public void dealHand(){
+		InitTourneyColours();
+		e.dealHand();
+		System.out.print(e.getPlayerById(11).getHandSize());
+		assertEquals(8, e.getPlayerById(11).getHandSize());
+	}
 }
