c80ca1b60703052dc2cccab6bc4dc7d9c2a2dfde
Shray <Shray@192.168.0.18>
2016-03-03 23:24:07 -0500

    Type 1 Action card handler shells

--- src/comp3004/ivanhoe/ActionCardsEngine.java
+++ src/comp3004/ivanhoe/ActionCardsEngine.java
@@ -1,7 +1,40 @@
 package comp3004.ivanhoe;
 
+import comp3004.ivanhoe.Card.CardColour;
+
 public class ActionCardsEngine {
 	
+	/**
+	 * General action card handler
+	 * @param card Card 
+	 * @param caster player playing action card
+	 * @param players players that are effected by the card
+	 */
+	public void actionHandler(Card card, Player caster,  Player... players){
+		
+		switch(card.getCardName()){
+		case "Unhorse":
+			unhorse(caster);
+			break;
+		case "Change Weapon":
+			changeWeapon(caster);
+			break;
+		case "Drop Weapon":
+			dropWeapon(caster);
+			break;
+		}
+	}
 	
+	public void unhorse(Player caster){
+		
+	}
+	
+	public void changeWeapon(Player caster){
+		
+	}
+	
+	public void dropWeapon(Player caster){
+		
+	}
 	
 }
