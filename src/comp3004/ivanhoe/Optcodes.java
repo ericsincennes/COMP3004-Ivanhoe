d35aa33770642662aa064667df0f519c05da52f3
Shray <Shray@192.168.0.18>
2016-03-06 00:20:01 -0500

    Added Server functions to handle optcodes

--- src/comp3004/ivanhoe/Optcodes.java
+++ src/comp3004/ivanhoe/Optcodes.java
@@ -4,12 +4,12 @@ public class Optcodes {
 	//Server to Client Opt codes
 	public static final int ClientGetHand 			= 101;
 	public static final int ClientupdateBoardState 	= 102;
-	public static final int ClientFirstTournament 	= 103;
+	public static final int ClientGetCardsToBePlayed= 103;
 	public static final int ClientGetColourChoice 	= 104;
 	
 	//Client to Server Opt codes
 	public static final int ServerSendColourChoice 	= 201;
-	public static final int ServerGetCardsToBePlayed= 202;
+	//public static final int ServerGetCardsToBePlayed= 202;
 	public static final int ServerWithdrawClient  	= 203;
 	public static final int ServerEndClientTurn	 	= 204;
 }
