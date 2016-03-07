97586ffcb0801aeff08e416da1da8c58d765cd76
Mike Zhang <ifarmpandas@gmail.com>
2016-03-07 02:26:42 -0500

    v2

--- src/comp3004/ivanhoe/RulesEngine.java
+++ src/comp3004/ivanhoe/RulesEngine.java
@@ -260,7 +260,9 @@ public class RulesEngine {
 	
 	public boolean giveToken(long id, CardColour c) {
 		Player p = getPlayerById(id);
-		if (c!= null && (TournamentColour.equals(CardColour.Purple) || c.equals(TournamentColour))) {
+		if (TournamentColour == null) { print("wtf why is tourney colour null?"); }
+		if (c!= null && TournamentColour != null
+				&& (TournamentColour.equals(CardColour.Purple) || c.equals(TournamentColour))) {
 			p.recieveToken(c);
 			return true;
 		}
