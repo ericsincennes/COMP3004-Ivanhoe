bfa935728570e956d58b0f46e8df6ac5d18dc154
Mike Zhang <ifarmpandas@gmail.com>
2016-03-19 16:18:50 -0400

    fixed missing space in squire name

--- src/comp3004/ivanhoe/SupporterCard.java
+++ src/comp3004/ivanhoe/SupporterCard.java
@@ -4,6 +4,6 @@ public class SupporterCard extends ColourCard {
 	public SupporterCard (int value) {
 		super(CardColour.White, value);
 		cardType = CardType.Supporter;
-		cardName = (value == 2 || value == 3) ? "Squire" + String.valueOf(value) : (value == 6) ? "Maiden" : "";
+		cardName = (value == 2 || value == 3) ? "Squire " + String.valueOf(value) : (value == 6) ? "Maiden" : "";
 	}
 }
