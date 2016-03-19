0af41db22e4e6f66a148638cced61e71b9e854b8
Shray <Shray@192.168.0.18>
2016-03-19 14:56:13 -0400

    Merge Conflict

--- src/comp3004/ivanhoe/Server.java
+++ src/comp3004/ivanhoe/Server.java
@@ -188,7 +188,7 @@ public class Server{
 								}
 								long winner = rules.withdrawCleanup(threadID); //now its winner's turn
 								cardIndex = -3;
-								
+
 							} else if(cardIndex == -3) { //end turn optcode received
 								rules.endTurn(threadID);
 								cardIndex = -3;
@@ -200,228 +200,228 @@ public class Server{
 						}
 					}
 					else {
-					if (!rules.isColourChosen()) {
-						//choose colour
-						if (rules.canStartTournament(threadID)) {
-							CardColour c;
-							c = GetTournamentColourFromClient();
-							while(!rules.initializeTournamentColour(threadID, c)) {
+						if (!rules.isColourChosen()) {
+							//choose colour
+							if (rules.canStartTournament(threadID)) {
+								CardColour c;
+								c = GetTournamentColourFromClient();
+								while(!rules.initializeTournamentColour(threadID, c)) {
+									//send some message about bad colour input
+									c = GetTournamentColourFromClient();
+								}	
+							}
+							else {
+								rules.failInitTournamentColour();
+							}
+						}
+						//Send updated hand to client
+						SendClientHand();
+
+						//get what cards the client wants to play
+						int cardIndex = getCardsToBePlayed();
+						while(cardIndex != -3){ //while not endturn optcode
+							cardIndex = getCardsToBePlayed();
+
+							if(cardIndex == -2){ //Client withdrawing
+								if (rules.withdrawPlayer(threadID)) {
+									CardColour c = GetTournamentColourFromClient();
+									rules.getPlayerById(threadID).removeToken(c); //may need validation
+								}
+								long winner = rules.withdrawCleanup(threadID); //now its winner's turn, they'll get a choice of token when their loop hits code
+
+								cardIndex = -3;
+							} else if(cardIndex == -3) { //end turn optcode received
+								rules.endTurn(threadID);
+								cardIndex = -3;
+							} else if(cardIndex != -1){
+								rules.playCard(cardIndex, threadID);
+								SendClientHand();
+								updateClientBoardState();
+							}
+						}
+
+					}
+					else {
+						if (rules.getPlayerById(threadID).getPlaying()) { //then you are winner of previous tourney
+							CardColour c = GetTournamentColourFromClient();
+							while(!rules.giveToken(threadID, c)) {
 								//send some message about bad colour input
 								c = GetTournamentColourFromClient();
-							}	
-						}
-						else {
-							rules.failInitTournamentColour();
-						}
-					}
-					//Send updated hand to client
-					SendClientHand();
-
-					//get what cards the client wants to play
-					int cardIndex = getCardsToBePlayed();
-					while(cardIndex != -3){ //while not endturn optcode
-						cardIndex = getCardsToBePlayed();
-
-						if(cardIndex == -2){ //Client withdrawing
-							if (rules.withdrawPlayer(threadID)) {
-								CardColour c = GetTournamentColourFromClient();
-								rules.getPlayerById(threadID).removeToken(c); //may need validation
 							}
-							long winner = rules.withdrawCleanup(threadID); //now its winner's turn, they'll get a choice of token when their loop hits code
-							
-							cardIndex = -3;
-						} else if(cardIndex == -3) { //end turn optcode received
-							rules.endTurn(threadID);
-							cardIndex = -3;
-						} else if(cardIndex != -1){
-							rules.playCard(cardIndex, threadID);
-							SendClientHand();
-							updateClientBoardState();
 						}
+						rules.initTournament();
 					}
-
 				}
-				else {
-					if (rules.getPlayerById(threadID).getPlaying()) { //then you are winner of previous tourney
-						CardColour c = GetTournamentColourFromClient();
-						while(!rules.giveToken(threadID, c)) {
-							//send some message about bad colour input
-							c = GetTournamentColourFromClient();
-						}
+			}
+
+
+
+			/**
+			 * Gets the displays for all players and sends it to the client
+			 * Currently does not send ActionCards in the display
+			 */
+			private void updateClientBoardState(){
+				ArrayList<List<Card>> board = new ArrayList<List<Card>>();
+				List<Card> nonActionCards = rules.getPlayerById(threadID).getDisplay().getCards();
+				//List<Card> actionCards = rules.getPlayerById(threadID).getDisplay().getActionCards();
+
+				board.add(nonActionCards);
+
+				for( Player p : rules.getPlayerList()){
+					if(p.getID() != threadID){
+						board.add(p.getDisplay().getCards());
 					}
-					rules.initTournament();
 				}
+
+				send(Optcodes.ClientupdateBoardState);
+				send(board);
 			}
-		}
 
-
-
-		/**
-		 * Gets the displays for all players and sends it to the client
-		 * Currently does not send ActionCards in the display
-		 */
-		private void updateClientBoardState(){
-			ArrayList<List<Card>> board = new ArrayList<List<Card>>();
-			List<Card> nonActionCards = rules.getPlayerById(threadID).getDisplay().getCards();
-			//List<Card> actionCards = rules.getPlayerById(threadID).getDisplay().getActionCards();
-
-			board.add(nonActionCards);
-
-			for( Player p : rules.getPlayerList()){
-				if(p.getID() != threadID){
-					board.add(p.getDisplay().getCards());
+			/**
+			 * Checks if the client sent the withdraw optcode
+			 * @param opt Integer
+			 * @return Boolean
+			 */
+			private boolean isClientWithdrawing(int opt){
+				if(opt == Optcodes.ClientWithdraw){
+					return true;
 				}
-			}
-
-			send(Optcodes.ClientupdateBoardState);
-			send(board);
-		}
-
-		/**
-		 * Checks if the client sent the withdraw optcode
-		 * @param opt Integer
-		 * @return Boolean
-		 */
-		private boolean isClientWithdrawing(int opt){
-			if(opt == Optcodes.ClientWithdraw){
-				return true;
-			}
-			return false;
-		}
-
-		/**
-		 * Get the index of the card to be played and plays the card
-		 */
-		private int getCardsToBePlayed(){
-			send(Optcodes.ClientGetCardsToBePlayed);
-			int index = (int) get();
-			String cardname = "";
-
-			if(isClientWithdrawing(index)){ //Client withdraws
-				return -2;
-			}
-			if (index == Optcodes.ClientEndTurn){ //client calls end turn
-				return -3;
-			}
-
-			if(index < rules.getPlayerById(threadID).getHandSize()){
-				cardname = rules.getPlayerById(threadID).getHand().getCardbyIndex(index).getCardName();
-			}
-
-
-			//if client sent a card
-			if(cardname != ""){
-				if(rules.validatePlay(cardname, threadID)){
-					send(Optcodes.SuccessfulCardPlay);
-					return index;
-				} else {
-					send(Optcodes.InvalidCard);
-					return -1;
-				}
-			}
-			return -1;
-		}
-
-		/**
-		 * Sends the thread id representing the player in the rules engine to the player
-		 */
-		private void sendPlayerId(){
-			//Register Thread with the rules engine
-			int b = rules.registerThread(threadID);
-
-			//if game full close connection
-			if(b != -1){ 
-				send(b); 
-			} else {
-				isRunning = false;
-			}
-		}
-
-		private void sendPlayersList() {
-			int me_index = 0;
-			int listSize = rules.getPlayerList().size();
-			List<Long> playerIDs = new ArrayList<Long>();
-			for (; me_index < listSize; me_index++) {
-				if (rules.getPlayerList().get(me_index).getID() == threadID) {
-					break;
-				}
-			}
-			for (int i=0; i < listSize; i++) {
-				playerIDs.add(rules.getPlayerList().get((me_index+i)%listSize).getID());
-			}
-			send(Optcodes.ClientGetPlayerList);
-			send(playerIDs);
-
-		}
-
-		/**
-		 * Gets the hand from the Player class and sends it to the client
-		 */
-		private void SendClientHand(){
-			print("Thread " + threadID + ": Sending hand to client");
-			List<Card> hand = rules.getPlayerById(threadID).getHand().getHand();
-			send(Optcodes.ClientGetHand);
-			send(hand);
-		}
-
-		/**
-		 * Gets the tournament colour from the client
-		 * @return CardColour
-		 */
-		private CardColour GetTournamentColourFromClient(){
-			print("Thread " + threadID + ": getting tournament colour from client");
-			send(Optcodes.ClientGetColourChoice);
-			CardColour colour = null;
-
-			int o = (int) get(); //get colour from client
-
-			switch (o) {
-			case 1: colour = Card.CardColour.Purple;
-			break;
-			case 2: colour = Card.CardColour.Green;
-			break;
-			case 3: colour = Card.CardColour.Red;
-			break;
-			case 4: colour = Card.CardColour.Blue;
-			break;
-			case 5: colour = Card.CardColour.Yellow;
-			break;
-			default:
-				break;
-			}
-
-			return colour;
-		}
-
-		/**
-		 * Gets an object from the client
-		 * Does not verify the typeOf an object
-		 * @return
-		 */
-		private Object get(){
-			Object o = new Object();
-			try {
-				o = in.readObject();
-			} catch (ClassNotFoundException | IOException e) {
-				e.printStackTrace();
-			}
-			return o;
-		}
-
-		/**
-		 * Sends an object to the client
-		 * @param o Object to be sent
-		 * @return boolean if successful
-		 */
-		private boolean send(Object o){
-			try {
-				out.writeObject(o);
-				out.flush();
-			} catch (IOException e) {
-				e.printStackTrace();
 				return false;
 			}
-			return true;
+
+			/**
+			 * Get the index of the card to be played and plays the card
+			 */
+			private int getCardsToBePlayed(){
+				send(Optcodes.ClientGetCardsToBePlayed);
+				int index = (int) get();
+				String cardname = "";
+
+				if(isClientWithdrawing(index)){ //Client withdraws
+					return -2;
+				}
+				if (index == Optcodes.ClientEndTurn){ //client calls end turn
+					return -3;
+				}
+
+				if(index < rules.getPlayerById(threadID).getHandSize()){
+					cardname = rules.getPlayerById(threadID).getHand().getCardbyIndex(index).getCardName();
+				}
+
+
+				//if client sent a card
+				if(cardname != ""){
+					if(rules.validatePlay(cardname, threadID)){
+						send(Optcodes.SuccessfulCardPlay);
+						return index;
+					} else {
+						send(Optcodes.InvalidCard);
+						return -1;
+					}
+				}
+				return -1;
+			}
+
+			/**
+			 * Sends the thread id representing the player in the rules engine to the player
+			 */
+			private void sendPlayerId(){
+				//Register Thread with the rules engine
+				int b = rules.registerThread(threadID);
+
+				//if game full close connection
+				if(b != -1){ 
+					send(b); 
+				} else {
+					isRunning = false;
+				}
+			}
+
+			private void sendPlayersList() {
+				int me_index = 0;
+				int listSize = rules.getPlayerList().size();
+				List<Long> playerIDs = new ArrayList<Long>();
+				for (; me_index < listSize; me_index++) {
+					if (rules.getPlayerList().get(me_index).getID() == threadID) {
+						break;
+					}
+				}
+				for (int i=0; i < listSize; i++) {
+					playerIDs.add(rules.getPlayerList().get((me_index+i)%listSize).getID());
+				}
+				send(Optcodes.ClientGetPlayerList);
+				send(playerIDs);
+
+			}
+
+			/**
+			 * Gets the hand from the Player class and sends it to the client
+			 */
+			private void SendClientHand(){
+				print("Thread " + threadID + ": Sending hand to client");
+				List<Card> hand = rules.getPlayerById(threadID).getHand().getHand();
+				send(Optcodes.ClientGetHand);
+				send(hand);
+			}
+
+			/**
+			 * Gets the tournament colour from the client
+			 * @return CardColour
+			 */
+			private CardColour GetTournamentColourFromClient(){
+				print("Thread " + threadID + ": getting tournament colour from client");
+				send(Optcodes.ClientGetColourChoice);
+				CardColour colour = null;
+
+				int o = (int) get(); //get colour from client
+
+				switch (o) {
+				case 1: colour = Card.CardColour.Purple;
+				break;
+				case 2: colour = Card.CardColour.Green;
+				break;
+				case 3: colour = Card.CardColour.Red;
+				break;
+				case 4: colour = Card.CardColour.Blue;
+				break;
+				case 5: colour = Card.CardColour.Yellow;
+				break;
+				default:
+					break;
+				}
+
+				return colour;
+			}
+
+			/**
+			 * Gets an object from the client
+			 * Does not verify the typeOf an object
+			 * @return
+			 */
+			private Object get(){
+				Object o = new Object();
+				try {
+					o = in.readObject();
+				} catch (ClassNotFoundException | IOException e) {
+					e.printStackTrace();
+				}
+				return o;
+			}
+
+			/**
+			 * Sends an object to the client
+			 * @param o Object to be sent
+			 * @return boolean if successful
+			 */
+			private boolean send(Object o){
+				try {
+					out.writeObject(o);
+					out.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+				return true;
+			}
 		}
 	}
-}
